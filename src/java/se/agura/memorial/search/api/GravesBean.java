46c9fd10422097f7a839bfc644fc9f52e271644e
Igors Sidrovs <Igors.Sidorovs@idega.lv>
2005-06-01 14:05:07 +0000

    Refactoring

--- src/java/se/agura/memorial/search/api/GravesBean.java
+++
@@ -1,9 +0,0 @@
-package se.agura.memorial.search.api;
-
-
-import com.idega.business.IBOServiceBean;
-
-
-public class GravesBean  extends IBOServiceBean {
-
-}
--- src/java/se/agura/memorial/search/api/GravesHome.java
+++
@@ -1,7 +0,0 @@
-package se.agura.memorial.search.api;
-
-import com.idega.business.IBOHome;
-
-public interface GravesHome extends IBOHome{
-
-}
