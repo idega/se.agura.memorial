8d0f9cd3ba12e63a4f5d9525ef7c9e7c017e4373
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2005-06-02 08:39:58 +0000

    implemented getGraveyards() in a buggy way

--- src/java/se/agura/memorial/search/impl/LocalObituarySearch.java
+++ src/java/se/agura/memorial/search/impl/LocalObituarySearch.java
@@ -1,9 +1,17 @@
 package se.agura.memorial.search.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import se.agura.memorial.search.api.Grave;
+import se.agura.memorial.search.api.Graveyard;
 import se.agura.memorial.search.api.ObituarySearch;
+import se.agura.memorial.search.data.GraveGraveyard;
+import se.agura.memorial.search.data.GraveGraveyardHome;
+
+import com.idega.data.IDOLookup;
 
 public class LocalObituarySearch implements ObituarySearch {
 
@@ -17,8 +25,38 @@ public class LocalObituarySearch implements ObituarySearch {
 	}
 
 	public Collection getGraveyards() {
-		// TODO make this work using GraveCemetery
-		return null;
+		// TODO make this work using GraveGraveyard
+		List result = new ArrayList();
+		
+		GraveGraveyardHome ggh = null;	            
+        try {
+			ggh = (GraveGraveyardHome) IDOLookup.getHome(GraveGraveyard.class);
+			Collection coll = ggh.findAll();
+			
+			Iterator it = coll.iterator();
+			while (it.hasNext()) {
+				Object o = it.next();
+
+				GraveGraveyard g = (GraveGraveyard) o;
+				
+				
+				result.add(new Graveyard(
+						1,//()g.getPrimaryKey(),
+						g.getGraveyardName(),
+						g.getGraveyardName(),
+						1));
+
+			}				
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("error occured while getting graveyards:");
+			e.printStackTrace();
+		}
+		
+	
+		
+		return result;
 	}
 
 	public Grave findGrave(String graveId) {
