56b8d116bd2c12738e02fe8b506ec88668e46a24
Igors Sidrovs <Igors.Sidorovs@idega.lv>
2005-06-02 07:54:05 +0000

    Updated dateFromString

--- src/java/se/agura/memorial/search/util/Utility.java
+++ src/java/se/agura/memorial/search/util/Utility.java
@@ -11,12 +11,16 @@ public class Utility {
 	public static Date stringToDate(String s){
 		Date date = null;
 		
+		if (s == null) return null;
+		
+		if (s.length()!=8) return null;
 	    try {
 			DateFormat formatter = new SimpleDateFormat("yyyymmdd");
 			//TODO  what will happen if date is like this: 200301
 	        date = (Date)formatter.parse(s);
 	    } catch (ParseException e) {
-	    }		
+	    }	
+		
 		
 		return date;
 	}
